7fb4537496d9144ca6d0b7c18a7bb837b0726c8c
Lance Ennen <byennen@gmail.com>
2010-04-24 22:39:03 -0500

    refactored js added prices deal url and image

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,10 @@
 window.onload = function() {
-  var deal, titleDiv;
+  var deal, titleDiv,  priceDiv, imageDiv, buynowDiv;
+
   deal = GrouponDeals.get().topDeal;
-  titleDiv = document.getElementById("title");
-  titleDiv.innerHTML = deal.title;
+  titleDiv = document.getElementById("title").innerHTML = deal.title;
+  priceDiv = document.getElementById("price").innerHTML = deal.price;
+	imageDiv = document.getElementById("image").innerHTML = "<img src=" + deal.medium_image_url + ">";
+	buynowDiv = document.getElementById("buy-now").innerHTML = "<a href="+ deal.deal_url + ">Buy Now</a>";
+
 };
